cba5a8efbb7351d665b442262e50acf417368ae0
Faris-07 <farisuddin03@gmail.com>
2022-02-14 13:13:34 +0000

    Create place_ship function for computer and player to place ships on the board and checks if ships fit and dosent overlap

--- run.py
+++ run.py
@@ -31,6 +31,49 @@ def print_board(board):
         row_number += 1
 
 
+def place_ship(board):
+    """
+    The place ship function loops throught the lengths of the ships and then
+    loops until the ship fits and dosent overlap any other ships on the board
+    """
+    #  loop through length of ships
+    for ship_length in SHIP_LENGTHS:
+        #  loop until ship fits and doesn't overlap
+        while True:
+            if board == COMPUTER_BOARD:
+                orientation, row, column = random.choice(["H", "V"]), \
+                    random.randint(0, 7), random.randint(0, 7)
+                if fit_ship_check(ship_length, row, column, orientation):
+                    #  check if ship overlaps
+                    if not ship_overlap(board, row, column, orientation,
+                    ship_length):
+                        #  place ship
+                        if orientation == "H":
+                            for i in range(column, column + ship_length):
+                                board[row][i] = "X"
+                        else:
+                            for i in range(row, row + ship_length):
+                                board[i][column] = "X"
+                        break
+            else:
+                place_ship = True
+                print('Place the ship with a length of ' + str(ship_length))
+                row, column, orientation = user_input(place_ship)
+                if fit_ship_check(ship_length, row, column, orientation):
+                    # check if ship overlaps
+                    if not ship_overlap(board, row, column, orientation,
+                    ship_length):
+                        # place ship
+                        if orientation == "H":
+                            for i in range(column, column + ship_length):
+                                board[row][i] = "X"
+                        else:
+                            for i in range(row, row + ship_length):
+                                board[i][column] = "X"
+                            print_board(PLAYER_BOARD)
+                            break
+
+
 def fit_ship_check(SHIP_LENGTH, row, column, orientation):
     """
     The fit_ship_check checks if the ships inputted fit on the board
