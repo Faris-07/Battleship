dced009345015db60c2220bdfa07263b20d729e4
Faris-07 <farisuddin03@gmail.com>
2022-02-14 12:06:50 +0000

    Declared PLAYER_GUESS_BOARD

--- run.py
+++ run.py
@@ -1,2 +1,3 @@
 PLAYER_BOARD = [[" "] * 8 for i in range(8)]
-COMPUTER_BOARD = [[" "] * 8 for i in range(8)]  
+COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
+PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]  
