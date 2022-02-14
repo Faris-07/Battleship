95f022b24aa0e2fb3176644a56a285603b6431e9
Faris-07 <farisuddin03@gmail.com>
2022-02-14 12:07:33 +0000

    Declared COMPUTER_GUESS_BOARD variable

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 PLAYER_BOARD = [[" "] * 8 for i in range(8)]
 COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
 PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]  
+COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
