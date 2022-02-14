a215f39f199cffe73c0a1ef4624fd96f14c59b17
Faris-07 <farisuddin03@gmail.com>
2022-02-14 12:12:55 +0000

    Create letters_conversion dictionary to store letters to numbers conversion

--- run.py
+++ run.py
@@ -3,6 +3,17 @@ COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
 PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]  
 COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
 
+letters_conversion = {
+    'A': 0,
+    'B': 1,
+    'C': 2,                                           
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
+
 
 def print_board(board):
     """
