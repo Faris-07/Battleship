b995f031cfeb300bb8e348d41039a48ebe891540
Faris-07 <farisuddin03@gmail.com>
2022-02-14 12:09:40 +0000

    Create print_board function which prints out the battleship board

--- run.py
+++ run.py
@@ -2,3 +2,15 @@ PLAYER_BOARD = [[" "] * 8 for i in range(8)]
 COMPUTER_BOARD = [[" "] * 8 for i in range(8)]
 PLAYER_GUESS_BOARD = [[" "] * 8 for i in range(8)]  
 COMPUTER_GUESS_BOARD = [[" "] * 8 for i in range(8)]
+
+
+def print_board(board):
+    """
+    The print_board function prints out the battleship board
+    """
+    print("  A B C D E F G H")
+    print("  ---------------")     
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
